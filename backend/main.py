c7a239b82f90250b90cbb206bee1387111ac34f5
Jan <Jan.Sellerbeck@Vodafone.com>
2024-03-10 20:30:05 +0100

    first running config added favicon to frontend/assets

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,23 @@
 import json
 import logging
-import os
-import sys
-from datetime import date
-from html import escape
-from flask import Flask, render_template, request, redirect, url_for, make_response
+from flask import Flask, render_template, request, make_response, redirect, send_from_directory
 
 # create flask app
-app = Flask(__name__, static_folder='static', template_folder='templates')
+app = Flask(__name__, static_folder='../frontend/dist/assets', template_folder='../frontend/dist')
 
 
 @app.route('/')
 def index():
-    resp = make_response(render_template("index.html"))
-    return resp
+    return send_from_directory(app.template_folder, 'index.html')
 
 
 @app.route('/api/v1/post_form', methods=["POST"]) ##Datum; Rechnungsnummer; Name; Liste von Artikeln (Artikelnummer	Beschreibung	Verwendung	Kostenstelle	Preis (€))
 def post_form():
-    date = request.form["Datum"]
-    bill_nr = request.form["Rechnungsnummer"]
-    name = request.form["name"]
-    resp = make_response(render_template("index.html"))
-    return resp
+    if request.method == "POST" and '/' in request.referrer:
+        logging.debug("POST request from root")
+    redirect("/")
 
 
 if __name__ == '__main__':
+    logging.debug("Starting app")
     app.run(debug=True, host="0.0.0.0", port="8080")
